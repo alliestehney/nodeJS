9a7e2db36535c16f2d73e0b1490deecacd71326f
alliestehney <alliestehney1@gmail.com>
2017-11-06 13:20:29 -0500

    Server runs and displays random task within browser

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var http = require("http");
 
+var mod = require("./tasks.js");
+
+var taskList = mod.tasks;
+console.log(taskList);
+
+for (var i=0; i<taskList.length; i++) {
+	console.log(taskList[i]);
+}
+
+var randomNumber = Math.floor(Math.random() *4); // generates a random number between 1 and 4
+
 http.createServer(function(request, response) {
 	response.writeHead(200, { "Content-type": "text/plain"});
-	response.write("I like mint chocolate chip ice cream.");
+	response.write(taskList[randomNumber]);
 	response.end();
 }).listen(3000);
---
+++ tasks.js
@@ -0,0 +1,3 @@
+var tasks = ["Clean the kitchen", "Cook dinner", "Go grocery shopping", "Fill up the car with gas"];
+
+module.exports.tasks = tasks;
